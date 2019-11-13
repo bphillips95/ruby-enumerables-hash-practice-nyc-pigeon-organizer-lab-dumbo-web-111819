54fc7cc6b4c7be49faafd71433bef81e831a5240
Baruch Phillips <baruchphillips@gmail.com>
2019-11-13 19:27:59 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,13 +6,11 @@ pigeon_list = {}
   value.each do |stats,all_names| 
   all_names.each do |names| 
     # .each loops through the Hash
- if pigeon_list[names] == nil
-          pigeon_list[names] = {}
-        end
-        if pigeon_list[names][color_gender_lives] == nil
-          pigeon_list[names][color_gender_lives] = []
-        end
+      pigeon_list[name] ||= {}
+        pigeon_list[name][attribute_name] ||= []
+     
         # in order to create the Hash you have to first check if it exists??
+   
      pigeon_list[names][color_gender_lives].push(stats.to_s)
 # stats is a Hash in its original state
   end 
